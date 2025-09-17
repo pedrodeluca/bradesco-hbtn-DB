af514b04a34d9b0d5007422010942cf6e7915b1e
Pedro H. Deluca <pedrodluk@gmail.com>
2025-09-17 01:25:36 +0000

    subindo schema das tabelas

---
+++ jpa_hibernate/sql_schema_database_admin.sql
@@ -0,0 +1,65 @@
+{
+"pessoas": [
+	{
+		"id" : 1,
+		"cpf" : 12345678910,
+		"dataDeNascimento" : "30\/07\/1987",
+		"email" : "pedrodluk@email.com",
+		"idade" : 38,
+		"nome" : "Pedro H Deluca"
+	},
+	{
+		"id" : 3,
+		"cpf" : 12345678910,
+		"dataDeNascimento" : "30\/07\/1984",
+		"email" : "vanessa@email.com",
+		"idade" : 40,
+		"nome" : "Vanessa L C Deluca"
+	}
+]}
+
+{
+"produtos": [
+	{
+		"id" : 1,
+		"nome" : "Tablet",
+		"preco" : 280.0,
+		"quantidade" : 120,
+		"status" : 0
+	},
+	{
+		"id" : 3,
+		"nome" : "Notebook",
+		"preco" : 1400.0,
+		"quantidade" : 10,
+		"status" : 0
+	},
+	{
+		"id" : 4,
+		"nome" : "TV",
+		"preco" : 300.0,
+		"quantidade" : 100,
+		"status" : 1
+	},
+	{
+		"id" : 5,
+		"nome" : "Notebook",
+		"preco" : 1400.0,
+		"quantidade" : 10,
+		"status" : 0
+	},
+	{
+		"id" : 6,
+		"nome" : "TV",
+		"preco" : 300.0,
+		"quantidade" : 100,
+		"status" : 1
+	},
+	{
+		"id" : 7,
+		"nome" : "Notebook",
+		"preco" : 1400.0,
+		"quantidade" : 10,
+		"status" : 0
+	}
+]}
