6c2b2135063955d73c92afc672a0befb6c6a268d
Pedro H. Deluca <pedrodluk@gmail.com>
2025-10-06 13:21:54 +0000

    Incluindo construtor classe endereco

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Endereco.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Endereco.java
@@ -10,13 +10,30 @@ public class Endereco {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    private String logradouro;
     private String endereco;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
 
     @ManyToOne
     @JoinColumn(name = "aluno_id")
     private Aluno aluno;
 
     public Endereco() {
+    }
+
+    public Endereco(long id, String logradouro, String endereco, int numero, String bairro, String cidade, String estado, String cep) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.endereco = endereco;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
     }
 
     public Endereco(String endereco) {
@@ -50,5 +67,53 @@ public class Endereco {
     @Override
     public String toString() {
         return this.endereco;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
     }
 }
