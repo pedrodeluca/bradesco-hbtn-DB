f19d46aa39f2f548b674c6457a4a58b167479c56
Pedro H. Deluca <pedrodluk@gmail.com>
2025-10-06 13:24:21 +0000

    Incluindo construtor classe professor

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
@@ -11,15 +11,42 @@ public class Professor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private String nome;
+    private String nomeCompleto;
+
+    private String matricula;
+
+    private String email;
 
     @OneToMany
     @JoinColumn(name = "professor_id")
     private List<Curso> cursos;
 
     public Professor(String nome, List<Curso> curso) {
-        this.nome = nome;
+        this.nomeCompleto = nome;
         this.cursos = curso;
+    }
+
+    public Professor(long id, String nomeCompleto, String matricula, String email) {
+        this.id = id;
+        this.nomeCompleto = nomeCompleto;
+        this.matricula = matricula;
+        this.email = email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public void setId(long id) {
@@ -29,12 +56,12 @@ public class Professor {
         return id;
     }
 
-    public String getNome() {
-        return nome;
+    public String getNomeCompleto() {
+        return nomeCompleto;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
     }
 
     public List<Curso> getCursos() {
